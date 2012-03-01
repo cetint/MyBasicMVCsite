3fca2835f822e3951299d562aeaa19f7365efc64
tamercetin <cetintamer.itu@gmail.com>
2012-03-01 11:29:07 +0200

    First MVC project added

---
+++ MyFirstMVC/MyFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyFirstMVC.Models;
+
+namespace MyFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyFirstMVC/MyFirstMVC/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MyFirstMVC.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage="please enter a valid email adress")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+
+        
+    }
+
+}
